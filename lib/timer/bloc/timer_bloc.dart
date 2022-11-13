7799698effea58ed75d36d3a356f34616629cab2
JuliaMod <julmodlinski@gmail.com>
2022-11-13 11:19:59 +0100

    add intial duration

--- lib/timer/bloc/timer_bloc.dart
+++ lib/timer/bloc/timer_bloc.dart
@@ -4,8 +4,9 @@ part 'timer_event.dart';
 part 'timer_state.dart';
 
 class TimerBloc extends Bloc<TimerEvent, TimerState> {
-  //TODO: set inital state
-  TimerBloc() : super() {
+  static const int _duration = 60;
+  
+  TimerBloc() : super(TimerInitial(_duration)) {
     //TODO: implement eventhandlers
   }
 }
