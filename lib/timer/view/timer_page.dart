30437501efede4bfb2d809412c5729a2cf7004df
JuliaMod <julmodlinski@gmail.com>
2022-11-13 11:57:14 +0100

    add timer page

---
+++ lib/timer/view/timer_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timer_bloc/timer/bloc/timer_bloc.dart';
+
+class TimerPage extends StatelessWidget {
+  const TimerPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+        create: (_) => TimerBloc(ticker: Ticker()),
+      child: const TimerView(),
+    );
+  }
+}
+
+class TimerView extends StatelessWidget {
+  const TimerView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter Timer'),),
+      body: Stack(
+        children: [
+          const Background(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 100.0),
+                child: Center(child: TimerText(),),
+              ),
+              Actions()
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class TimerText extends StatelessWidget {
+  const TimerText({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final duration = context.select((TimerBloc bloc) => bloc.state.duration);
+    final minutesStr =
+    ((duration / 60) % 60).floor().toString().padLeft(2, '0');
+    final secondStr = (duration % 60).floor().toString().padLeft(2, '0');
+    return Text(
+      '$minutesStr:$secondStr',
+      style: Theme.of(context).textTheme.headline1,
+    );
+  }
+}
+
