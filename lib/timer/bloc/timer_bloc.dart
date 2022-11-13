4dbb70be3703ba863f2dbefab482b008ed37cc0b
JuliaMod <julmodlinski@gmail.com>
2022-11-13 11:18:58 +0100

    add timer bloc

---
+++ lib/timer/bloc/timer_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+
+part 'timer_event.dart';
+part 'timer_state.dart';
+
+class TimerBloc extends Bloc<TimerEvent, TimerState> {
+  //TODO: set inital state
+  TimerBloc() : super() {
+    //TODO: implement eventhandlers
+  }
+}
